45a8d5ac9525f95f0bad5adc9f2bb13623cceb5b
Julien Blin <julien@alleycorpnord.com>
2024-03-06 17:36:43 -0500

    Update seeding - rebased from latest changes.

--- medplum/seed.js
+++ medplum/seed.js
@@ -15,28 +15,29 @@ async function seedDatabase() {
         logger_1.globalLogger.info('Already seeded');
         return;
     }
+    const systemRepo = (0, repo_1.getSystemRepo)();
     const [firstName, lastName, email] = ['Medplum', 'Admin', 'admin@example.com'];
     const passwordHash = await (0, utils_1.bcryptHashPassword)('medplum_admin');
-    const superAdmin = await repo_1.systemRepo.createResource({
+    const superAdmin = await systemRepo.createResource({
         resourceType: 'User',
         firstName,
         lastName,
         email,
         passwordHash,
     });
-    const superAdminProject = await repo_1.systemRepo.createResource({
+    const superAdminProject = await systemRepo.createResource({
         resourceType: 'Project',
         name: 'Super Admin',
         owner: (0, core_1.createReference)(superAdmin),
         superAdmin: true,
         strictMode: true,
     });
-    await repo_1.systemRepo.updateResource({
+    await systemRepo.updateResource({
         resourceType: 'Project',
         id: exports.r4ProjectId,
         name: 'FHIR R4',
     });
-    const practitioner = await repo_1.systemRepo.createResource({
+    const practitioner = await systemRepo.createResource({
         resourceType: 'Practitioner',
         meta: {
             project: superAdminProject.id,
@@ -55,7 +56,7 @@ async function seedDatabase() {
             },
         ],
     });
-    await repo_1.systemRepo.createResource({
+    await systemRepo.createResource({
         resourceType: 'ProjectMembership',
         project: (0, core_1.createReference)(superAdminProject),
         user: (0, core_1.createReference)(superAdmin),
@@ -63,7 +64,7 @@ async function seedDatabase() {
         admin: true,
     });
     // Add Default project
-    const defaultProject = await repo_1.systemRepo.updateResource({
+    const defaultProject = await systemRepo.updateResource({
         resourceType: 'Project',
         id: 'cd8a377a-ab31-4dc2-8c71-ba18a4888a18',
         name: 'Default',
@@ -71,7 +72,7 @@ async function seedDatabase() {
         strictMode: true,
         superAdmin: false,
     });
-    const defaultProjectPractitioner = await repo_1.systemRepo.createResource({
+    const defaultProjectPractitioner = await systemRepo.createResource({
         resourceType: 'Practitioner',
         meta: {
             project: defaultProject.id,
@@ -90,14 +91,14 @@ async function seedDatabase() {
             },
         ],
     });
-    await repo_1.systemRepo.createResource({
+    await systemRepo.createResource({
         resourceType: 'ProjectMembership',
         project: (0, core_1.createReference)(defaultProject),
         user: (0, core_1.createReference)(superAdmin),
         profile: (0, core_1.createReference)(defaultProjectPractitioner),
         admin: true,
     });
-    const clientApp = await repo_1.systemRepo.updateResource({
+    const clientApp = await systemRepo.updateResource({
         resourceType: 'ClientApplication',
         id: 'f54370de-eaf3-4d81-a17e-24860f667912',
         meta: {
@@ -107,7 +108,7 @@ async function seedDatabase() {
         secret: '75d8e7d06bf9283926c51d5f461295ccf0b69128e983b6ecdd5a9c07506895de',
         redirectUri: process.env.INITIAL_CLIENT_APP_REDIRECT_URI || 'http://localhost:3000/',
     });
-    await repo_1.systemRepo.createResource({
+    await systemRepo.createResource({
         resourceType: 'ProjectMembership',
         project: (0, core_1.createReference)(defaultProject),
         user: (0, core_1.createReference)(clientApp),
@@ -124,6 +125,7 @@ exports.seedDatabase = seedDatabase;
  * @returns True if already seeded.
  */
 function isSeeded() {
-    return repo_1.systemRepo.searchOne({ resourceType: 'User' });
+    const systemRepo = (0, repo_1.getSystemRepo)();
+    return systemRepo.searchOne({ resourceType: 'User' });
 }
 //# sourceMappingURL=seed.js.map
